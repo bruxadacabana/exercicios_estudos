342cf610d364920da3131024a4c41912b1ce5e1e
Jenifer Angelo <jenmangelo@gmail.com>
2022-09-14 14:17:34 -0300

    ee

---
+++ Faculdade/2FPR - Fundamentos de Programação/exercicios/struct.c
@@ -0,0 +1,26 @@
+// importação de biblioteca
+#include <stdio.h>
+
+//constantes?
+
+//tipo??
+typedef struct{
+    char nome[30];
+    float av1, av2, media;
+    char situacao[10];
+} TAluno;
+
+// cabeçalhos das funções
+
+void leitura(TAluno turma[], int quantAl);
+
+//main
+
+void main(){
+    TAluno turma[30];
+}
+
+// ler, 
+void leitura(TAluno turma[], int quantAl){
+
+}
