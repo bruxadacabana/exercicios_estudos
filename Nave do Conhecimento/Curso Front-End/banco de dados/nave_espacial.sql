51447dc61b133a05826ab9c95636457e8725a8d3
Jenifer Angelo <jenmangelo@gmail.com>
2022-09-16 19:25:31 -0300

    ughh

--- Nave do Conhecimento/Curso Front-End/banco de dados/nave_espacial.sql
+++ Nave do Conhecimento/Curso Front-End/banco de dados/nave_espacial.sql
@@ -1,15 +1,32 @@
 CREATE DATABASE nave_espacial;
-CREATE TABLE usuarios (
-	nome varchar(100),
+CREATE TABLE dependentes(
+    nome varchar(100),
+    usuarioid varchar(20)
+);
+CREATE TABLE usuarios(
+    nome varchar(100),
     cpf varchar(11),
-    idUsuario int(15)
+    idusuario varchar(20)
 );
-CREATE TABLE espacos (
-    idEspaco int(16),
-    nomeEspaco varchar(100),
-    usuariosEspaco varchar(255),
-    turmasEspaco int(16)
+CREATE TABLE espacos(
+    idespaco varchar(20),
+    nomeespaco varchar(50),
+    usuariosespaco,
+    turmasespaco
 );
-CREATE TABLE turma (
-	
+CREATE TABLE turma(
+    idturma,
+    nometurma,
+    datainicio,
+    datafim,
+    idcurso
 );
+CREATE TABLE curso{
+    idcurso,
+    materia,
+    ementa
+};
+CREATE TABLE alunocurso(
+    idusuario,
+    idturma
+)
