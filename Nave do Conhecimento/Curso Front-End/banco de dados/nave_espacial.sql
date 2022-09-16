4cd1a2f3acc686e9f34132fa6a6109d0e8022109
Jenifer Angelo <jenmangelo@gmail.com>
2022-09-16 19:31:24 -0300

    hmm

--- Nave do Conhecimento/Curso Front-End/banco de dados/nave_espacial.sql
+++ Nave do Conhecimento/Curso Front-End/banco de dados/nave_espacial.sql
@@ -11,22 +11,22 @@ CREATE TABLE usuarios(
 CREATE TABLE espacos(
     idespaco varchar(20),
     nomeespaco varchar(50),
-    usuariosespaco,
-    turmasespaco
+    usuariosespaco int(100),
+    turmasespaco int(100)
 );
 CREATE TABLE turma(
-    idturma,
-    nometurma,
-    datainicio,
-    datafim,
-    idcurso
+    idturma varchar(20),
+    nometurma varchar(100),
+    datainicio DATE,
+    datafim DATE,
+    idcurso varchar(20)
 );
 CREATE TABLE curso{
-    idcurso,
-    materia,
-    ementa
+    idcurso varchar(20),
+    materia varchar(50),
+    ementa text(400)
 };
 CREATE TABLE alunocurso(
-    idusuario,
-    idturma
+    idusuario varchar(20),
+    idturma varchar(20)
 )
