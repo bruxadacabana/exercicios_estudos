5cea3c8a863ad248e68e47e102009b6ea7d436d5
Jenifer Angelo <jenmangelo@gmail.com>
2022-09-16 19:45:34 -0300

    acho que ta indo hein

--- Nave do Conhecimento/Curso Front-End/banco de dados/nave_espacial.sql
+++ Nave do Conhecimento/Curso Front-End/banco de dados/nave_espacial.sql
@@ -1,32 +1,38 @@
 CREATE DATABASE nave_espacial;
 CREATE TABLE dependentes(
     nome varchar(100),
-    usuarioid varchar(20)
+    usuarioid int
 );
 CREATE TABLE usuarios(
     nome varchar(100),
     cpf varchar(11),
-    idusuario varchar(20)
+    idusuario int
 );
 CREATE TABLE espacos(
-    idespaco varchar(20),
+    idespaco int,
     nomeespaco varchar(50),
-    usuariosespaco int(100),
-    turmasespaco int(100)
+    usuariosespaco int,
+    turmasespaco int
 );
 CREATE TABLE turma(
-    idturma varchar(20),
+    idturma int,
     nometurma varchar(100),
-    datainicio DATE,
-    datafim DATE,
-    idcurso varchar(20)
+    datainicio DATETIME,
+    datafim DATETIME,
+    idcurso int
 );
-CREATE TABLE curso{
-    idcurso varchar(20),
+CREATE TABLE curso(
+    idcurso int,
     materia varchar(50),
-    ementa text(400)
-};
+    ementa text
+);
 CREATE TABLE alunocurso(
-    idusuario varchar(20),
-    idturma varchar(20)
-)
+    idusuario int,
+    idturma int
+);
+
+INSERT INTO usuarios (nome, cpf)
+VALUES ('Kaio', 11111111111);
+
+INSERT INTO curso (materia, ementa)
+VALUES ('Python', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi venenatis pulvinar orci, at consequat justo consectetur sed. Proin varius mollis fermentum. Cras risus lacus, commodo a viverra quis, sodales et nisl. Quisque ligula libero, varius id molestie ut, pulvinar sed eros. Maecenas cursus, erat sed rhoncus efficitur, sapien metus facilisis turpis, ac pellentesque nunc nulla gravida ipsum. Etiam dignissim ultricies felis, non efficitur eros mollis sit amet. Aenean gravida, turpis non rutrum sodales, felis diam feugiat orci, et mollis tortor tortor sagittis diam.');
