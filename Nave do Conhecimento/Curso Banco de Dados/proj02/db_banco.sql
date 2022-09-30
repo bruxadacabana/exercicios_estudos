c68dff2000836919be5f7e2f35c9e47f2aa4e6ac
Jenifer Angelo <jenmangelo@gmail.com>
2022-09-30 19:49:47 -0300

    by

--- Nave do Conhecimento/Curso Banco de Dados/proj02/db_banco.sql
+++ Nave do Conhecimento/Curso Banco de Dados/proj02/db_banco.sql
@@ -1,23 +1,20 @@
 CREATE TABLE `pessoas` (
-  `id_pessoa` int(11) DEFAULT NULL AUTO_INCREMENT,
-  `primeiro_nome` varchar(255) DEFAULT NULL,
-  `sobrenome` varchar(255) DEFAULT NULL,
+  `id_pessoa` int(15) DEFAULT NULL AUTO_INCREMENT,
+  `primeiro_nome` varchar(25) DEFAULT NULL,
+  `sobrenome` varchar(100) DEFAULT NULL,
   `data_nascimento` date DEFAULT NULL,
   `endereço` varchar(255) DEFAULT NULL,
-  `Identidade` int(11) DEFAULT NULL,
+  `Identidade` int(9) DEFAULT NULL,
   `cpf` int(11) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `telefone` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (id_pessoa),
-  FOREIGN KEY (id_pessoa) REFERENCES cliente(id_cliente),
-  FOREIGN KEY (id_pessoa) REFERENCES funcionarios(id_funcionario),
-  FOREIGN KEY (id_pessoa) REFERENCES gerente(id_gerente)
+  `email` varchar(100) DEFAULT NULL,
+  `telefone` varchar(14) DEFAULT NULL,
+  PRIMARY KEY (id_pessoa)
 );
 
 CREATE TABLE `conta` (
-  `id_conta` int(11) DEFAULT NULL AUTO_INCREMENT,
-  `saldo` int(11) DEFAULT NULL,
-  `extrato` int(11) DEFAULT NULL,
+  `id_conta` int(15) DEFAULT NULL AUTO_INCREMENT,
+  `saldo` int(15) DEFAULT NULL,
+  `extrato` varchar(255) DEFAULT NULL,
   `tipo_conta` varchar(15) DEFAULT NULL,
   `senha` varchar(20) DEFAULT NULL,
   `serviços` varchar(255) DEFAULT NULL,
@@ -25,32 +22,36 @@ CREATE TABLE `conta` (
 );
 
 CREATE TABLE `cliente` (
-  `id_cliente` int(11) DEFAULT NULL,
-  `conta` int(11) DEFAULT NULL,
-  `agencia` int(11) DEFAULT NULL,
+  `id_cliente` int(15) DEFAULT NULL AUTO_INCREMENT,
+  'id_pessoa' int(15) DEFAULT NULL,
+  `conta` int(15) DEFAULT NULL,
+  `agencia` int(6) DEFAULT NULL,
+  FOREIGN KEY (id_pessoa) REFERENCES pessoas(id_pessoa),
   PRIMARY KEY (id_cliente)
 );
 
 
 CREATE TABLE `cliente_conta` (
-  `id_cliente` int(11) DEFAULT NULL,
-  `id_conta` int(11) DEFAULT NULL,
-  PRIMARY KEY (id_cliente),
-  PRIMARY KEY (id_conta),
+  `id_cliente` int(15) DEFAULT NULL,
+  `id_conta` int(15) DEFAULT NULL,
   FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente),
-  FOREIGN KEY (id_conta) REFERENCES conta(id_conta),
+  FOREIGN KEY (id_conta) REFERENCES conta(id_conta)
 ); 
 
 CREATE TABLE `funcionarios` (
-  `id_funcionario` int(11) DEFAULT NULL,
-  `cargo` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (id_funcionario)
+  `id_funcionario` int(15) DEFAULT NULL,
+  'id_pessoa' int(15) DEFAULT NULL,
+  `cargo` varchar(75) DEFAULT NULL,
+  PRIMARY KEY (id_funcionario),
+  FOREIGN KEY (id_pessoa) REFERENCES pessoas(id_pessoa)
 );
 
 CREATE TABLE `gerente` (
-  `id_gerente` int(11) DEFAULT NULL,
+  `id_gerente` int(15) DEFAULT NULL,
+  'id_pessoa' int(15) DEFAULT NULL,
   `clientes` varchar(255) DEFAULT NULL,
-  `conta` int(11) DEFAULT NULL,
-  `cargo` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (id_gerente)
+  `conta` int(15) DEFAULT NULL,
+  `cargo` varchar(75) DEFAULT NULL,
+  PRIMARY KEY (id_gerente),
+  FOREIGN KEY (id_pessoa) REFERENCES pessoas(id_pessoa)
 );
