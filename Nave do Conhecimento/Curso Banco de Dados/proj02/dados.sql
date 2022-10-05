7194f1a40dd8c2032cb207a74cb752ff4f61496a
Jenifer Angelo <jenmangelo@gmail.com>
2022-10-05 19:20:04 -0300

    crud test

--- Nave do Conhecimento/Curso Banco de Dados/proj02/dados.sql
+++ Nave do Conhecimento/Curso Banco de Dados/proj02/dados.sql
@@ -5,9 +5,14 @@ CRUD -> CREATE, READ, UPDATE, DELETE
 
 /* C R E A T E */
 INSERT INTO `pessoas`(`primeiro_nome`, `sobrenome`, `data_nascimento`, `endereço`, `Identidade`, `cpf`, `email`, `telefone`) VALUES ('JM','Flora','2000-10-31','Algum canto de Nilópolis, Rio de Janeiro',111111111,11111111111,'jmflora@gmail.com','11111111');
+
 INSERT INTO `pessoas`(`primeiro_nome`, `sobrenome`, `data_nascimento`, `endereço`, `Identidade`, `cpf`, `email`, `telefone`) VALUES ('Sandy','Cristina','2001-06-31','Algum canto de São Paulo',222222222,22222222222,'sancristina@gmail.com','22222222');
+
 INSERT INTO `pessoas`(`primeiro_nome`, `sobrenome`, `data_nascimento`, `endereço`, `Identidade`, `cpf`, `email`, `telefone`) VALUES ('Camille','Mirabelle','0001-12-31','Faerie',666666666,66666666666,'millamirabello@gmail.com','66666666');
+
 INSERT INTO `pessoas`(`primeiro_nome`, `sobrenome`, `data_nascimento`, `endereço`, `Identidade`, `cpf`, `email`, `telefone`) VALUES ('John','Alan','2001-06-31','Cidade Pequena Fictícia',999999999,99999999999,'johnallan@gmail.com','99999999');
+
+INSERT INTO `pessoas`(`primeiro_nome`, `sobrenome`, `data_nascimento`, `endereço`, `Identidade`, `cpf`, `email`, `telefone`) VALUES ('John','Smith','2001-06-31','Cidade Pequena Fictícia',444444444,44444444444,'johnsm@gmail.com','44444444');
 
 
 /* R E A D */
@@ -15,6 +20,8 @@ SELECT * FROM `pessoas` WHERE id_pessoa=1;
 SELECT MIN(id_pessoa), primeiro_nome FROM `pessoas`;
 SELECT MAX(id_pessoa), primeiro_nome FROM `pessoas`;
 SELECT * FROM `pessoas` ORDER BY primeiro_nome;
+
+SELECT id_pessoa, primeiro_nome FROM pessoas /*WHERE condition*/ GROUP BY primeiro_nome ORDER BY primeiro_nome;
 
 
 
