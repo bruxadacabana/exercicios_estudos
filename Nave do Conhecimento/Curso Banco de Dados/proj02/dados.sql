c4c7ae7198c0395c94a07f619cf28fc978e22023
Jenifer Angelo <jenmangelo@gmail.com>
2022-10-05 19:21:29 -0300

    up

--- Nave do Conhecimento/Curso Banco de Dados/proj02/dados.sql
+++ Nave do Conhecimento/Curso Banco de Dados/proj02/dados.sql
@@ -26,7 +26,7 @@ SELECT id_pessoa, primeiro_nome FROM pessoas /*WHERE condition*/ GROUP BY primei
 
 
 /* U P D A T E */
-UPDATE
+UPDATE table_name SET column1 = value1, column2 = value2, ... WHERE condition;
 
 /* D E L E T E */
-DELETE
+DELETE FROM table_name WHERE condition;
