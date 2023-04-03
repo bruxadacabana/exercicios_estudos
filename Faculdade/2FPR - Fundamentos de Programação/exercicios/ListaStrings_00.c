5b0cd92c208077f7316a37c3a31575fa55d8f6ce
Jenifer Angelo <jenmangelo@gmail.com>
2023-04-03 18:52:16 -0300

    yaaas

--- Faculdade/2FPR - Fundamentos de Programação/exercicios/ListaStrings_00.c
+++ Faculdade/2FPR - Fundamentos de Programação/exercicios/ListaStrings_00.c
@@ -16,47 +16,49 @@ void main(){
     fflush(stdin);
     gets(s);
 
-    while (op != 6)
+    while (op != 7)
     {
         printf("\n\n--------------------------------------------");
         printf("\nO que voce deseja fazer?\n\n");
-        printf("1 - Determinar o numero de caracteres de uma string\n");
-        printf("2 - Determinar o numero de ocorrencias de um caracter em determinada string\n");
-        printf("3 - Verificar se uma string e um palindromo,\n");
-        printf("4 - Inverter uma string\n");
-        printf("5 - Remover todas as ocorrencias de determinado caracter em uma string\n");
-        printf("6 - Sair\n");
+        printf("1 - Exibir string\n");
+        printf("2 - Determinar o numero de caracteres de uma string\n");
+        printf("3 - Determinar o numero de ocorrencias de um caracter em determinada string\n");
+        printf("4 - Verificar se uma string e um palindromo,\n");
+        printf("5 - Inverter uma string\n");
+        printf("6 - Remover todas as ocorrencias de determinado caracter em uma string\n");
+        printf("7 - Sair\n");
         scanf("%d", &op);
 
-        switch (op)
-        {
-        case 1:
-            printf("Essa string tem %d caracteres", nTam(s));
-            break;
-        case 2:
-            printf("Qual caracter você deseja buscar? ");
-            scanf("%c", &sec);
-            printf("Esse caracter aparece %d vezes na string", nRep(s, sec));
-            break;
-        case 3:
-            /* code */
-            break;
-        case 4:
-            /* code */
-            break;
-        case 5:
-            printf("Qual caracter você deseja remover? ");
-            scanf("%c", &sec);
-            nRen(s, sec);
-            printf("Removido com sucesso");
-            printf("String atualizada: %s", s);
-            break;
-        case 6:
-            printf("\nTudo bem, obrigada por usar nossos servicos!");
-            break;
-        default:
-            printf("Comando invalido");
-            break;
+        switch (op){
+            case 1 :
+                break;
+            case 2:
+                printf("Essa string tem %d caracteres", nTam(s));
+                break;
+            case 3:
+                printf("Qual caracter você deseja buscar? ");
+                scanf(" %c", &sec);
+                printf("\nEsse caracter aparece %d vezes na string", nRep(s, sec));
+                break;
+            case 4:
+                /* code */
+                break;
+            case 5:
+                /* code */
+                break;
+            case 6:
+                printf("Qual caracter você deseja remover? ");
+                scanf(" %c", &sec);
+                nRen(s, sec);
+                printf("\nRemovido com sucesso");
+                printf("\nString atualizada: %s", s);
+                break;
+            case 7:
+                printf("\nTudo bem, obrigada por usar nossos servicos!");
+                break;
+            default:
+                printf("Comando invalido");
+                break;
         }
 
     }   
