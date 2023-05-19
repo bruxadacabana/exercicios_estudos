aca2cde9789e2d76fc3b0b73862c376dd7c1b4ca
Jenifer Angelo <jenmangelo@gmail.com>
2023-05-19 12:54:38 -0300

    .

--- Eu ProgrAmo/backend/ola.js
+++ Eu ProgrAmo/backend/ola.js
@@ -12,14 +12,14 @@ const porta = 3333
 function mostraOla(request, response){
     response.send("Olá, mundo!")
 }
-// "request" e "response" são os parâmetros para definir requisição e resposta
+// "request" e "response" são os parâmetros para definir requisição e resposta 
 
 function mostraPorta(){
     console.log("Servidor criado e rodando na porta ", porta)
 }
 
 app.use(router.get("/ola", mostraOla))
-//agora estamos dizendo para o aplicativo responder o verbo GET na rota /ola e chamar a função mostraOla
+// agora estamos dizendo para o aplicativo responder o verbo GET na rota /ola e chamar a função mostraOla
 
 app.listen(porta, mostraPorta)
-//após ouvir a porta, chama a função mostraPorta
+// após ouvir a porta, chama a função mostraPorta
