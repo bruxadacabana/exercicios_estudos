8cb94cf94665606ce6a9fb842b213b288b5ea03f
Jenifer Angelo <jenmangelo@gmail.com>
2023-05-20 17:50:12 -0300

    ugh

--- Faculdade/2FPR - Fundamentos de Programação/atividade01/atividade01.c
+++ Faculdade/2FPR - Fundamentos de Programação/atividade01/atividade01.c
@@ -9,9 +9,64 @@ Consequentemente, as colunas C+1 à última deverão serão deslocadas uma posi�
 #define COLUNA 5
 
 int moverColuna(int m[LINHA][COLUNA], int c);
+void exibir (int m[LINHA][COLUNA]);
+void gerar (int m[LINHA][COLUNA]);
 
 void main(){
-    
+    int c, resultado, matriz[LINHA][COLUNA];
+
+    gerar(matriz);
+    exibir(matriz);
+
+    printf("\nDigite o indice da coluna a ser movida: ");
+    scanf("%d", &c);
+
+    resultado = moverColuna(matriz, c);
+
+    if (resultado == 0){
+        printf("A coluna digitada não existe.");
+    } else{
+        printf("Feito com sucesso! Segue matriz atualizada: \n");
+        exibir(matriz);
+    }
 }
 
-int moverColuna(int m[LINHA][COLUNA], int c);
+int moverColuna(int m[LINHA][COLUNA], int c){
+    int i, vetor[COLUNA];
+
+    if (c >= COLUNA){
+        return 0;
+    }
+
+    copiarColuna()
+
+    for (i=0;)
+}
+
+void exibir (int m[LINHA][COLUNA]){
+    int i, j;
+	
+	printf ("\n\nElementos da matriz:\n");
+	for (i=0; i<LINHA; i++)
+	{
+		for (j=0; j<COLUNA; j++)
+		{
+			printf ("%d ", m[i][j]);
+		}
+		
+		printf ("\n");
+	}
+}
+
+void gerar (int m[LINHA][COLUNA]){
+    int i, j;
+	
+	srand(time(NULL));	
+	for (i=0; i<LINHA; i++)
+	{
+		for (j=0; j<COLUNA; j++)
+		{
+			m[i][j] = 1 + rand()%100;
+		}
+	}
+}
