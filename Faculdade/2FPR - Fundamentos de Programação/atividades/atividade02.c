1b1945a6734a1b2e25ea636fd5bf2536cf7ca923
Jenifer Angelo <jenmangelo@gmail.com>
2023-05-21 21:18:23 -0300

    lets go

--- Faculdade/2FPR - Fundamentos de Programação/atividades/atividade02.c
+++ Faculdade/2FPR - Fundamentos de Programação/atividades/atividade02.c
@@ -1,3 +1,10 @@
 /*
 Desenvolver uma função que, dada uma string str, substitua uma substring s1 de str (definida pelas posições inicial e final) por outra string, s2.
 */
+
+#include <stdio.h>
+#include <string.h>
+
+#define TOTAL 20
+
+void subst(char );
