a00a5a46013cbedc7f65d64a41174e480a14eca0
Jenifer Angelo <jenmangelo@gmail.com>
2023-05-27 20:10:44 -0300

    lets goo

--- letsCode_windows/helloWorld/src/Lojas/Petshop.java
+++ letsCode_windows/helloWorld/src/Lojas/Petshop.java
@@ -1,2 +1,21 @@
-package Lojas;public class Petshop {
+package Lojas;
+import animais.Animal;
+import animais.Cachorro;
+
+public class Petshop {
+
+    // esse método aceita como parametro qualquer animal
+    public void darBanho (Animal animal){
+        animal.setEstadoDeEspirito("limpo");
+    }
+
+    public void deixarNoHotel (Animal animal){
+        animal.setEstadoDeEspirito("com saudades");
+    }
+
+    // esse metodo aceita como parametro apenas cachorros
+    public void tosar(Cachorro cachorro){
+        cachorro.setEstadoDeEspirito("tosado");
+    }
+
 }
--- letsCode_windows/helloWorld/src/animais/Cachorro.java
+++ letsCode_windows/helloWorld/src/animais/Cachorro.java
@@ -1,2 +1,36 @@
-package animais;public class Cachorro {
+package animais;
+
+public class Cachorro extends Animal {
+    static int numeroDeCachorros;
+    private int tamanhoDoRabo;
+
+    public static int getNumeroDeCachorros(){
+        return numeroDeCachorros;
+    }
+
+    // MÉTODO CONSTRUTOR
+    public Cachorro(String nome, String cor, int altura, double peso){
+        this.nome = nome;
+        this.cor = cor;
+        this.altura = altura;
+        this.peso = peso;
+
+        numeroDeCachorros++;
+    }
+
+    // outros métodos definidos na classe pai
+
+    @Override
+    public String toString() {
+        return "Cachorro{" +
+                "nome='" + nome + '\'' +
+                '}';
+    }
+
+    @Override
+    public void soar() {
+        System.out.println("AU AU");
+    }
+
+
 }
