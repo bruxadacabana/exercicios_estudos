9cadc71864ca0dbdacd5ba509d920c71f220b384
Jenifer Angelo <jenmangelo@gmail.com>
2023-05-27 00:37:51 -0300

    lets go

--- Let's Code/helloworld/src/animais/Main.java
+++ Let's Code/helloworld/src/animais/Main.java
@@ -15,7 +15,20 @@ public class Main {
 		System.out.println("O cachorro está " + cachorro1.interagir("brincar"));
 		
 		System.out.println(cachorro1.getNome());
+		System.out.println("Endereço desse cachorro: " + cachorro1.toString());
+		
 		System.out.println(cachorro2.getNome());
+		System.out.println("Endereço desse cachorro: " + cachorro2.toString());
+		
+		cachorro2 = cachorro1;  //não atribui apenas valor, o objeto cachorro2 passa a apontar para o mesmo endereço que cachorro1
+		// garbage colector -> ele vai apagar os dados antigos de cachorro2, já que não já nenhum objeto apontando para aquele endereço mais
+		
+		
+		System.out.println(cachorro1.getNome());
+		System.out.println("Endereço desse cachorro: " + cachorro1.toString());
+		
+		System.out.println(cachorro2.getNome());
+		System.out.println("Endereço desse cachorro: " + cachorro2.toString());
 		
 	}
 
