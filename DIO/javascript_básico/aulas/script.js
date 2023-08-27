28d8f0eabb82b4058a679ee6a80526da94969ba8
Jenifer Angelo <jenmangelo@gmail.com>
2023-08-27 14:12:48 -0300

    starting the bootcamp!

---
+++ DIO/javascript_básico/aulas/script.js
@@ -0,0 +1,8 @@
+/* 
+var vouF = false;
+console.log(typeof(vouF));
+alert("Tudo certo!");*/
+
+let array = ['string', 1, true];
+console.log(array);
+array.forEach(function{item, index}{console.log{item, index}});
