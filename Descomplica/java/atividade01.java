de7f67f21ea1d0272a1e40a9038e37a46a96c2ff
Jenifer Angelo <jenmangelo@gmail.com>
2023-11-26 14:05:50 -0300

    Add code to declare and initialize variables, and receive user input

---
+++ Descomplica/java/atividade01.java
@@ -0,0 +1,12 @@
+/*Com os conhecimentos adquiridos até agora, desenvolva um programa em Java que declara variáveis inteiras, reais e String, recebe dois números inteiros usando interface com usuário, calcula o quociente da divisão dos dois números, a potência do primeiro número pelo segundo número e mostra essas informações usando interface com usuário.*/
+
+class Atividade{
+    public static void main (){
+        // variáveis
+        int a, b;
+        double c;
+        char d;
+        // entrada
+        a = Integer.parseInt(entrada)
+    }
+}
