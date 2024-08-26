daa4f383d9db27d09608e41e42b872e4ea1ae0be
Jenifer Angelo <jenmangelo@gmail.com>
2024-08-26 20:17:32 -0300

    feat: Add function to obtain base text for encryption

---
+++ alura/decodificador-texto/script.js
@@ -0,0 +1,8 @@
+function obterTextoBase(){
+    const input = document.getElementById('texto-base');
+    const texto = input.value;
+    const textoBase = texto.split("");
+    for (let i = 0; i <= textoBase.length; i++){
+        
+    }
+}
