a3512387f4ed8811008b00aa7ac4ef95553f6086
Jenifer Angelo <jenmangelo@gmail.com>
2024-08-26 21:42:15 -0300

    refactor: Refactor code to improve text decoding functionality

--- alura/decodificador-texto/script.js
+++ alura/decodificador-texto/script.js
@@ -1,28 +1,55 @@
-function obterTextoBase(){
+function converterParaArray(){
     const input = document.getElementById('texto-base');
     const texto = input.value;
     const textoBase = texto.split("");
-    for (let i = 0; i <= textoBase.length; i++){
-        if (textoBase[i] == "a"){
-            textoBase[i] = "ai"
-        }
-        if (textoBase[i] == "e"){
-            textoBase[i] = "enter"
-        }
-        if (textoBase[i] == "i"){
-            textoBase[i] = "imes"
-        }
-        if (textoBase[i] == "o"){
-            textoBase[i] = "ober"
-        }
-        if (textoBase[i] == "u"){
-            textoBase[i] = "ufat"
-        }
-    }
+    return textoBase;
+}
+
+function exibirResultado(textoBase){
     let textoConvertido = textoBase.join("");
     document.getElementById("principal__texto-resposta__texto").innerHTML = textoConvertido;
 }
 
+function obterTextoBase(){
+    let texto = converterParaArray();
+    for (let i = 0; i <= texto.length; i++){
+        if (texto[i] == "a"){
+            texto[i] = "ai"
+        }
+        if (texto[i] == "e"){
+            texto[i] = "enter"
+        }
+        if (texto[i] == "i"){
+            texto[i] = "imes"
+        }
+        if (texto[i] == "o"){
+            texto[i] = "ober"
+        }
+        if (texto[i] == "u"){
+            texto[i] = "ufat"
+        }
+    }
+    exibirResultado(texto);
+}
+
 function descrip(){
-    
+    let texto = converterParaArray();
+    for (let i = 0; i <= textoBase.length; i++){
+        if (texto[i] == "a"){
+            texto.splice(i+1,1)
+        }
+        if (texto[i] == "e"){
+            texto.splice(i+1,4)
+        }
+        if (texto[i] == "i"){
+            texto.splice(i+1,3)
+        }
+        if (texto[i] == "o"){
+            texto.splice(i+1,3)
+        }
+        if (texto[i] == "u"){
+            texto.splice(i+1,3)
+        }
+    }
+    exibirResultado(texto);
 }
