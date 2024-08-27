8dc3d7eff0451d8a11714102374fdeb27aa05975
Jenifer Angelo <jenmangelo@gmail.com>
2024-08-26 21:24:03 -0300

    chore: Add stylesheet link and update script in index.html

--- alura/decodificador-texto/script.js
+++ alura/decodificador-texto/script.js
@@ -3,6 +3,26 @@ function obterTextoBase(){
     const texto = input.value;
     const textoBase = texto.split("");
     for (let i = 0; i <= textoBase.length; i++){
-        
+        if (textoBase[i] == "a"){
+            textoBase[i] = "ai"
+        }
+        if (textoBase[i] == "e"){
+            textoBase[i] = "enter"
+        }
+        if (textoBase[i] == "i"){
+            textoBase[i] = "imes"
+        }
+        if (textoBase[i] == "o"){
+            textoBase[i] = "ober"
+        }
+        if (textoBase[i] == "u"){
+            textoBase[i] = "ufat"
+        }
     }
+    let textoConvertido = textoBase.join("");
+    document.getElementById("principal__texto-resposta__texto").innerHTML = textoConvertido;
+}
+
+function descrip(){
+    
 }
