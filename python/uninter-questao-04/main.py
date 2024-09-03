7a40cc865375e2ffeb41020b1d957a9624189a70
Jenifer Angelo <jenmangelo@gmail.com>
2024-09-03 20:55:38 -0300

    refactor: Improve user input validation in consultar_funcionarios function

--- python/uninter-questao-04/main.py
+++ python/uninter-questao-04/main.py
@@ -12,15 +12,29 @@ def cadastrar_funcionarios(id):
     lista_funcionarios.append(cad_funcionario)
 
 def consultar_funcionarios():
-    controle = 1
-    while controle != 4:
-        print("---------Menu de consulta---------")
+    while True:
+        print("------- --Menu de consulta---------")
         print("| 1 - Consultar todos            |")
         print("| 2 - Consultar por ID           |")
         print("| 3 - Consultar por setor        |")
         print("| 4 - Retornar ao menu principal |")
         print("|---------------------------------")
-        try controle = int(input(""))
+        try:
+            controle = int(input("Digite a opção desejada (1/2/3/4): "))
+        except ValueError:
+            print("Opção inválida, tente novamente.")
+            print("")
+            controle = 0
+            continue
+        if controle == 1:
+        elif controle == 2:
+        elif controle == 3:
+        elif controle == 4:
+            print("SAINDO DA FUNCAO")
+            return 0
+        else:
+            print("Opção inválida, tente novamente.")
+            print("")
 
 
 print("Bem-vindos à empresa da Jenifer Angelo Medeiros")
