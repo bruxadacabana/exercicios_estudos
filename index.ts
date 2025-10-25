5c26c4bbbbeda7c067cc6322c45a111a12edb2e9
Jenifer Angelo <jenmangelo@gmail.com>
2025-10-25 18:11:14 -0300

    feat: initialize learning project with Next.js, TypeScript, and Drizzle ORM
    - Added package.json with dependencies and scripts for Next.js and TypeScript.
    - Configured PostCSS and Prettier for styling and code formatting.
    - Created global CSS file with Tailwind CSS integration.
    - Implemented database connection setup using Drizzle ORM and PostgreSQL.
    - Established environment variable management with @t3-oss/env-nextjs.
    - Developed basic layout and homepage components for the application.
    - Included a script to start a local PostgreSQL database using Docker.
    - Defined TypeScript configuration for strict type checking and module resolution.
    - Added example TypeScript code demonstrating variable declarations and types.

---
+++ index.ts
@@ -0,0 +1,7 @@
+let age = 20; 
+let sales = 123456789;
+let course = 'Typescript';
+let is_published = true;
+let level; //type "any": significa que pode ser qualquer tipo
+let lista: number[];
+let user: [number, string] = [1, 'Mosh'];
